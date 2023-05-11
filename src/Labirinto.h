3e1443eeff319686ff11e78bff74d7232f462019
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2023-05-11 09:06:55 -0300

    Create Labirinto.h

---
+++ src/Labirinto.h
@@ -0,0 +1,24 @@
+#ifndef LABIRINTO_H
+#define LABIRINTO_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <ctype.h>
+
+typedef struct Matriz{
+char map[100][100];
+char mapAux[100][100];	
+	} Matriz;
+	
+	typedef struct Mapa{
+  Matriz Matrizes[QuantMatriz];
+	} Mapa;
+			
+void Initiation(Mapa *mat);
+int GenerateMatrix(int n,Mapa *mat);
+void PrintMatrix(int n,Mapa *mat);
+void ReadMatriz(Mapa *mat);
+int Random(int n,Mapa *mat);
+ 
+#endif
